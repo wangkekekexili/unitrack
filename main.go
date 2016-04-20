6ea8de4793423a4f8dac8f602759a8f37a9648e5
Ke Wang <wangkekekexili@gmail.com>
2016-04-20 19:46:26 -0400

    Test GPG.

--- main.go
+++ main.go
@@ -5,18 +5,37 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"strconv"
 )
 
+const (
+	defaultPort = "8080"
+)
+
+type trackingRequest struct {
+	vendor string
+	trackingNumber string
+}
+
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
+	port := getPort()
 	engine := gin.Default()
-	engine.GET("/ping", func(c *gin.Context) {
+
+	engine.GET("/uni/:tracking_number", func(c *gin.Context) {
+		trackingNumber := c.Param("tracking_number")
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"tracking_number": trackingNumber,
 		})
 	})
+
 	engine.Run(":" + port)
+}
+
+func getPort() string {
+	textPort := os.Getenv("PORT")
+	_, err := strconv.Atoi(textPort)
+	if err != nil {
+		return defaultPort
+	}
+	return textPort
 }
